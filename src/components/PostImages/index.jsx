20922ae8395c89621105fbe1e85a29a1ed4e301e
Raghavendra Sao <saoraghavendra@gmail.com>
2018-08-13 00:30:24 +0530

    image more text

--- src/components/PostImages/index.jsx
+++ src/components/PostImages/index.jsx
@@ -5,6 +5,7 @@ import './style.css'
 class PostImages extends Component {
   render() {
     const { imgUrls } = this.props
+    const len = imgUrls.length
     return (
       <div className="post__gallery flex__container">
         {imgUrls.slice(0, 2).map((url, index) => (
@@ -17,12 +18,13 @@ class PostImages extends Component {
         ))}
         {imgUrls.slice(2, 5).map((url, index) => (
           <div
-            className={`image__container m-t-10 ${
+            className={`image__container m-t-10 relative ${
               imgUrls.length === 3 ? 'flex__100' : imgUrls.length > 4 ? 'flex__32' : 'flex__49'
             }`}
             key={index + 2}
           >
             <img className="post__image" src={url} alt="img" />
+            {len > 5 && index === 2 && <div className="more__image__info">+ {len - 5} more</div>}
           </div>
         ))}
       </div>
